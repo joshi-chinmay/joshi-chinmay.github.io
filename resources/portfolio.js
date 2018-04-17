c6ff04e7302bba4ec5e26bd0066b29d6899aabdd
Chinmay <chinmay@amurtech.com>
2018-04-17 18:45:52 +0530

    blank and white theme

--- resources/portfolio.js
+++ resources/portfolio.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-  $('.content-welcome-overlay').delay(2600).fadeOut();
-  $('.container-portfolio').delay(100).fadeIn();
-
   $(".toggable-title").click(function(e) {
     $(e.currentTarget).toggleClass('active');
     $(e.currentTarget).closest(".tile-info-container").find(".tile-info-wrapper").toggle(1000);
