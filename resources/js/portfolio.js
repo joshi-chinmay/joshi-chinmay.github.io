2f4ecbdcab6562773d801708835b38d74f355487
Chinmay Joshi <chinmayj93@gmail.com>
2018-08-13 23:35:18 +0800

    night mode - day mode

--- resources/js/portfolio.js
+++ resources/js/portfolio.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+  // deside the mode of the website
+  var today = new Date().getHours();
+  if (today >= 7 && today <= 19) {
+    $("body").addClass("day-time");
+    $(".toggleable-icon").removeClass("fa-moon-o");
+    $(".toggleable-icon").addClass("fa-sun-o");
+  } else {
+    $("body").addClass("night-time");
+    $(".toggleable-icon").addClass("fa-moon-o");
+    $(".toggleable-icon").removeClass("fa-sun-o");
+  }
+
+  $(".day-night-mode-toggle").click(function(e) {
+    $("body").toggleClass("day-time");
+    $("body").toggleClass("night-time");
+
+    $(".toggleable-icon").toggleClass("fa-moon-o");
+    $(".toggleable-icon").toggleClass("fa-sun-o");
+  });
+
   $(".toggable-title").click(function(e) {
     var $container = $(e.currentTarget).closest(".portfolio-main-content");
     $currentActiveTile = $(e.currentTarget).closest(".tile-info-container");
