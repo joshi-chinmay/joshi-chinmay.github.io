57c9b2b14b9fd42aab3fc9ae63ab0c90a73298bf
joshi-chinmay <chinmayj93@gmail.com>
2018-10-31 21:58:11 +0800

    timer fix

--- resources/js/portfolio.js
+++ resources/js/portfolio.js
@@ -49,7 +49,7 @@ $(document).ready(function() {
       $(".welcome-note-container").hide();
       $(".main-page").show("450");
     });
-  }, 60);
+  }, 6000);
 });
 
 var optimizeMobileVersion = function() {
